ded70c44e0b63e0afea38552c5a57a9118b11fa0
Moataz Maher <Moatazmaher1998@gmail.com>
2020-11-07 12:18:41 +0200

    added main rout

--- Server/routes.js
+++ Server/routes.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-router.post('/user', function (req, res) {
-    console.log('\x1b[36m%s\x1b[0m','POST request on "/user" received on server');
-    res.send("User " + req.body.uname + " Logged in now");
+router.post('/main', function (req, res) {
+    console.log('\x1b[36m%s\x1b[0m','POST request on "/main" received on server');
+    res.sendFile(require('path').parse(__dirname).dir + '/Views/Main.html');
 });
 router.get('/', function (req, res) {
     console.log('\x1b[36m%s\x1b[0m','GET request on "/" received on server');
